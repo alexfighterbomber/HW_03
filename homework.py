9fd99713b32e1d97ce492a64c9cdab02c9ba7364
alexfighterbomber <ssamsobi@gmail.com>
2024-12-09 22:38:51 +0200

    get_numbers_ticket complete

--- homework.py
+++ homework.py
@@ -1,13 +1,9 @@
 from datetime import datetime
 
-def date_to_string(date):
-    return date.strftime("%Y.%m.%d")
-
-
 def get_days_from_today(date: str):                  # розрахунок кількості днів між date та поточною датою
     current_date = datetime.today().date()
     try:
-        target_date = datetime.strptime(date, "%Y-%m-%d").date()   # строку в формат datetime
+        target_date = datetime.strptime(date, "%Y-%m-%d").date()   # рядок в формат datetime
     except ValueError:
         return 'Невірний формат дати. Використовуйте "РРРР-ММ-ДД"'
     return (current_date - target_date).days
