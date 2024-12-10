47b6ad8436fab92255158efd960725e4d1fed5c7
alexfighterbomber <ssamsobi@gmail.com>
2024-12-10 14:36:12 +0200

    1

--- homework.py
+++ homework.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+import random
 
-def get_days_from_today(date: str):                  # розрахунок кількості днів між date та поточною датою
+def get_days_from_today(date: str)-> int:                  # розрахунок кількості днів між date та поточною датою
     current_date = datetime.today().date()
     try:
         target_date = datetime.strptime(date, "%Y-%m-%d").date()   # рядок в формат datetime
@@ -8,4 +9,13 @@ def get_days_from_today(date: str):                  # розрахунок кі
         return 'Невірний формат дати. Використовуйте "РРРР-ММ-ДД"'
     return (current_date - target_date).days
 
-print(get_days_from_today("2024-12-06"))
+def get_numbers_ticket(min=1, max=36, quantity=5):          # отримання quantity довільних унікальних чисел з діапазону min - max
+    if min < 1 or min > max-quantity or quantity > max-min or quantity < 1 or max > 1000 or min > max:
+        return "Невірні вхідні дані!"
+    numbers = set() #створюємо множину
+    while True:
+        numbers.add(random.randint(min, max)) # додаємо в неї довільні числа 
+        if len(numbers) >= quantity: break    # поки не наберемо кількість quantity
+    sorted_numbers = sorted(numbers)          # перетворюємо на сортований список
+    return sorted_numbers
+
